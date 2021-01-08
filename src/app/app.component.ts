b9bffe95000ba4817a2d23b030a163d9a9194e8d
HPA <htoopyaeaung.mobile@gmail.com>
2021-01-09 00:06:57 +0900

    [Add]HPA:Create New

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-
+import { AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask  } from 'angularfire2/storage';
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
+import { finalize } from 'rxjs/operators';
+import { map } from 'rxjs/operators/map';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-file-upload-firebase';
+  ref: AngularFireStorageReference;
+  task: AngularFireUploadTask;
+  uploadProgress: Observable<number>;
+  downloadURL: Observable<string>;
+
+  constructor(private afStorage: AngularFireStorage) { }
+
+  upload(event) {
+    const id = Math.random().toString(36).substring(2);
+    this.ref = this.afStorage.ref(id);
+    this.task = this.ref.put(event.target.files[0]);
+    this.uploadProgress = this.task.percentageChanges();
+    //this.downloadURL =  this.ref.getDownloadURL();
+    this.task.snapshotChanges().pipe(
+      finalize(() => this.downloadURL = this.ref.getDownloadURL() )
+    ).subscribe()
+
+
+
+  }
+
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: 'AIzaSyByTrI6i63tUSbnwlWyW10qcMF6grd5TzU',
+      authDomain: 'muc-media-server.firebaseapp.com',
+      databaseURL: 'https://muc-media-server-default-rtdb.firebaseio.com',
+      projectId: 'muc-media-server',
+      storageBucket: 'muc-media-server.appspot.com',
+      messagingSenderId: '460598162202',
+      appId: '1:460598162202:web:4c31baa609b00a2424b2a3',
+      measurementId: 'G-1PLV11X7RX'
+    }),
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
